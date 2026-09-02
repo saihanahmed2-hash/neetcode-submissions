ba24f8a338c60f15162cb1ad2748926cbf651296
saihanahmed2-hash <saihanahmed2@gmail.com>
2026-09-02 23:28:15 +0530

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    majorityElement(nums) {
+        const map=new Map()
+        for(let i=0;i<nums.length;i++){
+            if(map.has(nums[i])){
+                map.set(nums[i],map.get(nums[i])+1)
+            }else{
+                map.set(nums[i],1)
+            }
+        }
+        for(let [k,v] of map){
+            if(v>(nums.length/2)){
+                return k
+            }
+        }
+    }
+}
